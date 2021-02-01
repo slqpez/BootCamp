9055603182bfbdc6e69452bd467de6ce671b4eb0
slqpez <santiago.lopez16@udea.edu.co>
2021-02-01 12:15:35 -0500

    Props exercise.

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,14 +1,42 @@
 
-const Title = ({name, lastName})=>{
-  return <h1>{name} 
-  {lastName} La puta madre cómo soy así de imbécil.</h1>
+const Header =({course})=> <h1>{course}</h1>
+
+const Content = ({title, exercises}) =>{
+  return(
+    <>
+  <h2>{title}</h2>
+    <p>Exercises: {exercises}</p>
+</>)
 }
-function App() {
+const Total =({total})=>{
+  const suma = total.reduce((a,b)=>a+b)
+  return(
+    <p>Total exercises: {suma}</p>
+  )
+}
+
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  const totalEx = [exercises1,exercises2, exercises3]
+
   return (
-    <div className="App">
-      <Title  name="Santiago" lastName="López"></Title>
+    <div>
+      <Header course={course}></Header>
+      <Content title={part1} exercises={exercises1}></Content>
+      <Content title={part2} exercises={exercises2}></Content>
+      <Content title={part3} exercises={exercises3}></Content>
+      <Total total={totalEx}></Total>
+      
     </div>
-  );
-} 
+  )
+}
 
 export default App;
