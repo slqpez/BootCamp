e70d06d602c4d54baf67165b0b06fb1a88c07f5e
slqpez <santiago.lopez16@udea.edu.co>
2021-02-15 14:34:04 -0500

    Anecdotes step2.

--- anecdotes/src/App.js
+++ anecdotes/src/App.js
@@ -5,20 +5,45 @@ const Button = ({ handleClick, clicked }) => (
   <button onClick={handleClick}>{clicked ? "Next anecdote" : "Show anecdote"}</button>
 );
 
+const Vote =({handleClickVote})=>
+  <button onClick={handleClickVote}>Vote</button>
+
+
+const Anecdote =({anecdotes, clicked, selected})=> 
+clicked ? <p>{anecdotes[selected]}</p> : <></>
+
+
+
 const App = ({ anecdotes }) => {
   const [selected, setSelected] = useState(0);
   const [clicked, setClicked] = useState(false);
+  const [voteClicked, setVoteClicked] = useState(false);
+  const [votes, setVotes] = useState({0:0,1:0,2:0,3:0,4:0,5:0})
+
+  const handleClickVote=()=>{
+    const copy = {...votes}
+    copy[selected] +=1
+    setVotes({...copy})
+    setVoteClicked(true)
+    
+    
+}
   const showRandomAnecdote = () => {
     const min = 0;
     const max = anecdotes.length - 1;
     setSelected(Math.round(Math.random() * (max - min) + min));
     setClicked(true);
+    setVoteClicked(false)
   };
 
   return (
     <>
       <Button handleClick={showRandomAnecdote} clicked={clicked}></Button>
-      {clicked ? <p>{anecdotes[selected]}</p> : <p></p>}
+      <Anecdote clicked={clicked} anecdotes={anecdotes} selected={selected}></Anecdote>
+      
+      {clicked?<>
+      <p>Votes: {votes[selected]}</p>{!voteClicked?<Vote handleClickVote={handleClickVote}></Vote>:<></>}</>:<></>}
+     
     </>
   );
 };
