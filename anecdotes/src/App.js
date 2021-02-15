ef2a03bf772f029708b2c4714d9e071d925aad5c
slqpez <santiago.lopez16@udea.edu.co>
2021-02-15 12:05:27 -0500

    Anecdote paso1 fixed.

--- anecdotes/src/App.js
+++ anecdotes/src/App.js
@@ -1,28 +1,26 @@
-import './App.css';
-import {useState} from "react"
+import "./App.css";
+import { useState } from "react";
 
+const Button = ({ handleClick, clicked }) => (
+  <button onClick={handleClick}>{clicked ? "Next anecdote" : "Show anecdote"}</button>
+);
 
-
-const App = ({anecdotes}) => {
-  const [selected, setSelected] = useState(0)
-  const [clicked, setClicked] = useState(false)
-  const showRandomAnecdote =()=>{
-    const min=0
-    const max= anecdotes.length-1
+const App = ({ anecdotes }) => {
+  const [selected, setSelected] = useState(0);
+  const [clicked, setClicked] = useState(false);
+  const showRandomAnecdote = () => {
+    const min = 0;
+    const max = anecdotes.length - 1;
     setSelected(Math.round(Math.random() * (max - min) + min));
-    setClicked(true)
-    
-  }
+    setClicked(true);
+  };
 
   return (
     <>
-      <button onClick={showRandomAnecdote}>Show anecdote</button>
-      {clicked?<p>{anecdotes[selected]}</p>:<p></p>}
-      
+      <Button handleClick={showRandomAnecdote} clicked={clicked}></Button>
+      {clicked ? <p>{anecdotes[selected]}</p> : <p></p>}
     </>
-  )
-}
-
-
+  );
+};
 
 export default App;
