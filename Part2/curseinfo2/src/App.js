d9851389477c0d0eabf34663d4b9940433549943
slqpez <santiago.lopez16@udea.edu.co>
2021-02-21 17:23:16 -0500

    CourseInfo part2 step9.

--- Part2/curseinfo2/src/App.js
+++ Part2/curseinfo2/src/App.js
@@ -1,68 +1,88 @@
-import { React, useState } from "react";
+import { React } from "react";
 
-const Part = ({ text, exercises }) => {
-  return (
-    <>
-      <p>
-        {text}: <strong>{exercises}</strong>
-      </p>
-    </>
-  );
+const Part = ({ text, exercises }) => (
+  <li>
+    {text}: <strong>{exercises}</strong>
+  </li>
+);
+
+const Content = ({ parts }) =>
+  parts.map((part) => (
+    <Part key={part.id} text={part.name} exercises={part.exercises}></Part>
+  ));
+
+const Total = ({ parts }) => {
+  const totalExercisesByParts = parts.map((part) => part.exercises);
+  return <p>Total: {totalExercisesByParts.reduce((a, b) => a + b)}</p>;
 };
-const Total = ({exercises}) => <p>{exercises.reduce((a,b)=>a+b)}</p>;
 
 const Header = ({ text }) => <h1>{text}</h1>;
 
-const Content = ({ parts }) => {
+const Course = ({ courses }) => {
   return (
     <>
-      {parts.map((part) => (
-        <Part key={part.id} text={part.name} exercises={part.exercises}></Part>
-      ))}
-    </>
-  );
-};
-
-const Course = ({ course }) => {
-  return (
-    <>
-      <Header text={course.name}></Header>
+      {courses.map((course) => {
+        return (
+          <div key={course.id}>
+            <Header text={course.name}></Header>
+            <Content parts={course.parts}></Content>
+            <Total parts={course.parts}></Total>
+          </div>
+        );
+      })}
     </>
   );
 };
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      },
-    ],
-  };
-  const exercisesArray = course.parts.map(part=> part.exercises)
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
 
   return (
     <div className="App">
-      <Course course={course}></Course>
-      <Content parts={course.parts}></Content>
-      <Total exercises={exercisesArray}></Total>
-      
-      
-     
+      <Course courses={courses}></Course>
     </div>
   );
 };
