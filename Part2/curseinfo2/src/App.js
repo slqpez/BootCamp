03b5c18192d5674203393e3ff25461114e867841
slqpez <santiago.lopez16@udea.edu.co>
2021-02-21 10:51:16 -0500

    CourseInfo part2 step6.

---
+++ Part2/curseinfo2/src/App.js
@@ -0,0 +1,59 @@
+import { React, useState } from "react";
+
+const Part = ({ text, exercises }) => {
+  return (
+    <>
+      <p>{text}: <strong>{exercises}</strong></p> 
+    </>
+  );
+};
+const Header = ({ text }) => <h1>{text}</h1>;
+const Content = ({parts}) => {
+  return(
+    <>
+      {parts.map(part=> <Part key={part.id} text={part.name} exercises={part.exercises}></Part>)}
+
+    </>
+
+  )
+};
+
+const Course = ({ course }) => {
+  return (
+    <>
+      <Header text={course.name}></Header>
+    </>
+  );
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+  return (
+    <div className="App">
+      <Course course={course}></Course>
+      <Content parts={course.parts}></Content>
+    </div>
+  );
+};
+
+export default App;
