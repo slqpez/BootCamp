a136d280960ec4b581ec10ee62e35619fb4ac595
slqpez <santiago.lopez16@udea.edu.co>
2021-02-23 15:56:17 -0500

    Show all or importants.

--- notes/src/App.js
+++ notes/src/App.js
@@ -5,8 +5,9 @@ import Note  from "./components/Note"
 const App=({notes})=> {
   const [notesState, setNotes] = useState(notes)
   const [newNote, setNewNote] = useState("")
+  const [showAll, setShowAll] = useState(true)
 
-  const addNote = (event) => {
+  function addNote (event){
     event.preventDefault()
     const noteObject = {
       content: newNote,
@@ -21,16 +22,25 @@ const App=({notes})=> {
   function handleNoteChange(e){
     setNewNote(e.target.value)
   }
+  function handleShowAll(e){
+    e.preventDefault()
+    setShowAll((previusState=> !previusState))
+    console.log(showAll)
+  }
+
+  const notesToShow = showAll? notesState: notesState.filter(note=> note.important)
+
   return (
     <div className="App">
       <div>
       <h1>Notes</h1>
+      <button className="btn btn-show" onClick={handleShowAll}>Show {showAll?"importants":"all"}</button>
       <ul>
-       {notesState.map(note=> <Note key={note.id} note={note}></Note>)} 
+       {notesToShow.map(note=> <Note key={note.id} note={note}></Note>)} 
       </ul>
       <form onSubmit={addNote}>
       <input value={newNote} onChange={handleNoteChange}/>
-        <button type="submit">save</button>
+        <button className="btn btn-submit" type="submit">save</button>
       </form> 
     </div>
     </div>
