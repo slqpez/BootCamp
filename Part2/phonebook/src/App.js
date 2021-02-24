d383fbe55af22ac8daf07ec46756026243c43d47
slqpez <santiago.lopez16@udea.edu.co>
2021-02-23 20:24:45 -0500

    Phonebook step3.

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -1,47 +1,69 @@
-import React, { useState } from 'react'
-import './App.css';
+import React, { useState } from "react";
+import reactDom from "react-dom";
+import "./App.css";
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas',
-  id:1 }
-  ]) 
-  const [ newName, setNewName ] = useState('')
-   function handleSubmit(e){
-     e.preventDefault()
-     console.log('clicked')
-     const personObject={
-       name:newName,
-       id:persons.length +1
-     }
-     const nameExists = persons.find(person=> person.name === newName)
-     if(nameExists){
-       alert(`${newName} already exist.`)
-     }else{
-      setPersons([...persons, personObject])
-     }
-     
-     setNewName('')
-     
-   }
-   function handleInput(e){
-    setNewName(e.target.value)
-   }
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", id: 1, phone:"123456"}]);
+  const [newName, setNewName] = useState("");
+  const [newPhone, setNewPhone] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("clicked");
+    const personObject = {
+      name: newName,
+      id: persons.length + 1,
+      phone:newPhone
+    };
+    const nameExists = persons.find((person) => person.name === newName);
+    if(newPhone!== '' && newName!== ''){
+      if (nameExists) {
+        alert(`${newName} already exist.`);
+      } else {
+        if(!isNaN(newPhone)){
+          setPersons([...persons, personObject]);
+        }else{
+          alert("Por favor ingrese un número válido.")
+        }
+        
+      }
+
+    }else{
+      alert("Por favor llene todos los campos.")
+    }
+    
+
+    setNewName("");
+    setNewPhone("");
+  }
+  function handNameInput(e) {
+    setNewName(e.target.value);
+  }
+  function handlePhoneInput(e) {
+    setNewPhone(e.target.value);
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input onChange={handleInput} value={newName}/>
+          Name: <input onChange={handNameInput} value={newName} />
         </div>
         <div>
-          <button type="submit" onClick={handleSubmit}>add</button>
+          Phone: <input onChange={handlePhoneInput} value={newPhone} />
+        </div>
+        <div>
+          <button type="submit" onClick={handleSubmit}>
+            add
+          </button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person=><p key={person.id}>{person.name}</p>)}
+      {persons.map((person) => (
+        <p key={person.id}>{person.name} --- {person.phone}</p>
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default App;
