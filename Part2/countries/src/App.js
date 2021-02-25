48116b37551bb4019dbacc201b1e8c4f9e32b4fe
slqpez <santiago.lopez16@udea.edu.co>
2021-02-25 17:33:21 -0500

    Countries step1.

--- Part2/countries/src/App.js
+++ Part2/countries/src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from 'react'
+import './App.css'
+import axios from 'axios'
 
 function App() {
+  const [countries, setCountries] = useState([])
+  const [filterValue, setFilterValue] = useState("")
+
+  useEffect(() => {
+    axios.get("https://restcountries.eu/rest/v2/all").then(response=>{ 
+      setCountries(response.data)  
+    })
+  },[])
+
+  function handleFilterInput(e){
+    setFilterValue(e.target.value)
+
+  }
+
+  const CountryInfo=({country})=>{
+    return(
+      <div>
+        <h2>{country.name}</h2>
+        <p>Capital: {country.capital}</p>
+        <p>Population: {country.population}</p>
+        <h3>Languages</h3>
+        {country.languages.map(languaje=> <li key={languaje.iso639_1}>{languaje.name}</li>)}
+        <img  src={country.flag} alt="Flag" width="200" height="150"></img>
+      </div>
+    )
+  }
+
+  const Results =({results})=>{
+    if(results.length >10){
+     return(
+       <p>Too many matches, specify another filter.</p>
+     )
+    
+    }else if(results.length===1){
+      return <CountryInfo country={results[0]}></CountryInfo>
+    }else if(results.length<=10){
+      return(
+        results.map(countrie=>
+          <p key={countrie.alpha2Code}>{countrie.name}</p>
+        )
+      )
+    }
+  
+  }
+
+  const filteredCountries = countries.filter(countrie=> countrie.name.includes(filterValue))
+
+  
+  
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Find countries <input onChange={handleFilterInput} value={filterValue}></input>
+      <Results results={filteredCountries}></Results>
+    </div>
     </div>
   );
 }
