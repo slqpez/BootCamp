9f86429f61fdc945cbeb5df4eedb6ae994350724
slqpez <santiago.lopez16@udea.edu.co>
2021-02-28 12:57:47 -0500

    Phonebook step9-fixed.

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -1,88 +1,99 @@
-import React, { useState, useEffect} from "react";
-import Input from "./components/Input"
-import Form from "./components/Form"
-import Persons from "./components/Persons"
+import React, { useState, useEffect } from "react";
+import Input from "./components/Input";
+import Form from "./components/Form";
+import Persons from "./components/Persons";
 import "./App.css";
-import personService from "./services/names.service"
+import personService from "./services/names.service";
 
 const App = () => {
-  const [persons, setPersons] = useState([])
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newPhone, setNewPhone] = useState("");
   const [filterValue, setFilterValue] = useState("");
-  const [idSelected, setIdSelected] = useState(null);
 
-  useEffect(()=>{
-    personService.getAllNames().then(res=>{
-      setPersons(res.data)
-      setNewName("")
-    })
-    },[])
- 
-
+  useEffect(() => {
+    personService.getAllNames().then((res) => {
+      setPersons(res.data);
+      setNewName("");
+    });
+  }, []);
 
   function handleSubmit(e) {
     e.preventDefault();
     const personObject = {
       name: newName,
-      id: Math.max(persons.map(person=>person.id))+1,
-      number:newPhone
+      id: Math.max(persons.map((person) => person.id)) + 1,
+      number: newPhone,
     };
-    
+
     const nameExists = persons.find((person) => person.name === newName);
-    if(newPhone!== '' && newName!== ''){
+    if (newPhone !== "" && newName !== "") {
       if (nameExists) {
         alert(`${newName} already exist.`);
       } else {
-        if(!isNaN(newPhone)){
+        if (!isNaN(newPhone)) {
           setPersons([...persons, personObject]);
-          personService.addName(personObject).then(res=>{
-            setPersons([...persons, res.data])
-            setNewName("")
-          })
-        }else{
-          alert("Por favor ingrese un número válido.")
+          personService.addName(personObject).then((res) => {
+            setPersons([...persons, res.data]);
+            setNewName("");
+          });
+        } else {
+          alert("Por favor ingrese un número válido.");
         }
-        
       }
-
-    }else{
-      alert("Por favor llene todos los campos.")
+    } else {
+      alert("Por favor llene todos los campos.");
     }
-   
-    
 
     setNewName("");
     setNewPhone("");
   }
   function handNameInput(e) {
     setNewName(e.target.value);
-
   }
   function handlePhoneInput(e) {
     setNewPhone(e.target.value);
   }
-  function handleFilterInput(e){
-    setFilterValue(e.target.value)
+  function handleFilterInput(e) {
+    setFilterValue(e.target.value);
   }
-  function handleDelete(e){
-    const id =e.target.parentNode.getAttribute("data-id")
-    personService.deleteName(Number(id));
-    const newPersons = (persons.filter(person=>person.id !== Number(id)))
-    setPersons(newPersons)
+  function handleDelete(e) {
+    const id = e.target.parentNode.getAttribute("data-id");
+    const name = e.target.parentNode.getAttribute("data-name");
+    const confirm = window.confirm(`Do you wanna delete ${name}?`)
+    if(confirm){
+      personService.deleteName(Number(id));
+    const newPersons = persons.filter((person) => person.id !== Number(id));
+    setPersons(newPersons);
+    }
+    
   }
-  const filtPersons =persons.filter(person=>person.name.includes(filterValue))
+
+  const filtPersons = persons.filter((person) =>
+    person.name.includes(filterValue)
+  );
+  
 
   return (
     <div>
       <h2>Phonebook</h2>
       <div>
-       <Input labelText="Filter Show with" handleFunction={handleFilterInput} value={filterValue}></Input>
+        <Input
+          labelText="Filter Show with"
+          handleFunction={handleFilterInput}
+          value={filterValue}
+        ></Input>
       </div>
-      <Form handNameInput={handNameInput} handlePhoneInput={handlePhoneInput} handleSubmit={handleSubmit} valueName={newName} valuePhone={newPhone}></Form>
-      
+      <Form
+        handNameInput={handNameInput}
+        handlePhoneInput={handlePhoneInput}
+        handleSubmit={handleSubmit}
+        valueName={newName}
+        valuePhone={newPhone}
+      ></Form>
+
       <h2>Numbers</h2>
-       <Persons persons={filtPersons} handleDelete={handleDelete}></Persons>
+      <Persons persons={filtPersons} handleDelete={handleDelete}></Persons>
     </div>
   );
 };
--- Part2/phonebook/src/components/Persons.jsx
+++ Part2/phonebook/src/components/Persons.jsx
@@ -1,20 +1,11 @@
-import React, { useState, useEffect } from "react";
-import personService from "../services/names.service";
+
+
 
 const  Persons =  ({ persons, handleDelete }) => {
-  const [personFromComp, setPersonFromComp] =  useState([persons]);
-  /* function handleDelete(e, id) {
-    e.preventDefault();
-    personService.deleteName(id);
-    const newPersons = personFromComp.filter(person=>person.id===id)
-    //setPersonFromComp([newPersons])
-
-    
-  } */
   return(
     persons.map(person=>{
       return(
-        <div data-id={person.id} style={{ marginBottom: "1em" }} key={person.id}>
+        <div data-id={person.id} data-name={person.name} style={{ marginBottom: "1em" }} key={person.id}>
         <p style={{ display: "inline", marginRight: "1em" }}>
           <strong>{person.name}</strong> {person.number}
         </p>
